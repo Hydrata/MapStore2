4725b4a819d7c913d67da6726a7df48f82dd0def
davekennewell <davekennewell@gmail.com>
2020-11-09 14:25:54 +0400

    adding front-end plugin control

--- web/client/hydrata/HaitiBiblio/reducersHaitiBiblio.js
+++ web/client/hydrata/HaitiBiblio/reducersHaitiBiblio.js
@@ -9,7 +9,6 @@ const initialState = {
 export default ( state = initialState, action) => {
     switch (action.type) {
     case LOAD_FEATURE_INFO:
-        console.log('LOAD_FEATURE_INFO', state);
         return state;
     default:
         return state;
