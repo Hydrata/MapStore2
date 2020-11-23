2c1a3150df2bc91054f45ebf43d6f754432a07e0
davekennewell <davekennewell@gmail.com>
2020-11-23 13:36:44 +0400

    cleanup and add tooltips

--- web/client/hydrata/Swamm/components/swammBmpChart.js
+++ web/client/hydrata/Swamm/components/swammBmpChart.js
@@ -1,34 +1,11 @@
 import React from "react";
 import {connect} from "react-redux";
 const PropTypes = require('prop-types');
-import {Modal, Button, Table, ControlLabel, FormControl, FormGroup, Form, Col} from "react-bootstrap";
-import ReactDataGrid from 'react-data-grid';
-import Recharts from 'recharts';
-import { Toolbar, Data, Filters } from "react-data-grid-addons";
-import {formatMoney} from '../../Utils/utils';
+import {Modal, Button,  Col } from "react-bootstrap";
 import {hideSwammBmpChart} from "../actionsSwamm";
-const {
-    NumericFilter,
-    AutoCompleteFilter,
-    MultiSelectFilter,
-    SingleSelectFilter
-} = Filters;
-// const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
-const {Cell, LineChart, BarChart, RadialBarChart, RadialBar, PolarAngleAxis, PolarRadiusAxis, Line, Bar,
-    PieChart, Pie, ResponsiveContainer,
-    XAxis, YAxis, CartesianGrid, Tooltip, Legend} = require('recharts');
-const data = [
-    {id: '2', watershed: '1000', status: 'Proposed', type: 'Wetland', org: 'IEPA', n_red: 440, p_red: 2400, s_red: 0},
-    {id: '23', watershed: '2000', status: 'Proposed', type: 'Wascb', org: 'IEPA', n_red: 1234, p_red: 2400, s_red: 7831},
-    {id: '3', watershed: '2000', status: 'Operational', type: 'Filter Strips', org: 'IEPA', n_red: 4000, p_red: 2400, s_red: 4000},
-    {id: '4', watershed: '2000', status: 'Operational', type: 'Wetland', org: 'IEPA', n_red: 7831, p_red: 0, s_red: 2400},
-    {id: '5', watershed: '1000', status: 'Operational', type: 'Wetland', org: 'IEPA', n_red: 4536, p_red: 2400, s_red: 0},
-    {id: '6', watershed: '1000', status: 'Decomissioned', type: 'Wascb', org: 'Northwater', n_red: 4221, p_red: 7831, s_red: 222},
-    {id: '7', watershed: '3000', status: 'Decomissioned', type: 'Wascb', org: 'Northwater', n_red: 0, p_red: 2400, s_red: 122},
-    {id: '8', watershed: '3000', status: 'Proposed', type: 'Filter Strips', org: 'Northwater', n_red: 6879, p_red: 4000, s_red: 2221},
-    {id: '9', watershed: '3000', status: 'Proposed', type: 'Filter Strips', org: 'Northwater', n_red: 4533, p_red: 3667, s_red: 7866}
-];
-const data3 = [{
+const {Cell, BarChart, Bar, PieChart, Pie, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip} = require('recharts');
+
+const data = [{
     '2': {id: '2', watershed: '1000', status: 'Proposed', type: 'Wetland', org: 'IEPA', n_red: 440, p_red: 2400, s_red: 0},
     '23': {id: '23', watershed: '2000', status: 'Proposed', type: 'Wascb', org: 'IEPA', n_red: 1234, p_red: 2400, s_red: 7831},
     '3': {id: '3', watershed: '2000', status: 'Operational', type: 'Filter Strips', org: 'IEPA', n_red: 4000, p_red: 2400, s_red: 4000},
@@ -39,10 +16,11 @@ const data3 = [{
     '8': {id: '8', watershed: '3000', status: 'Proposed', type: 'Filter Strips', org: 'Northwater', n_red: 6879, p_red: 4000, s_red: 2221},
     '9': {id: '9', watershed: '3000', status: 'Proposed', type: 'Filter Strips', org: 'Northwater', n_red: 4533, p_red: 3667, s_red: 7866}
 }];
+
 const circleData = [
     { name: 'done', value: 55 },
-    { name: 'warning', value: 5 },
-    { name: 'todo', value: 40 }
+    { name: 'warning', value: 0 },
+    { name: 'todo', value: 45 }
 ];
 
 const colourMap = {
@@ -67,7 +45,6 @@ class SwammBmpChartClass extends React.Component {
 
     constructor(props) {
         super(props);
-        this.handleChange = this.handleChange.bind(this);
         this.state = {};
     }
 
@@ -95,12 +72,12 @@ class SwammBmpChartClass extends React.Component {
                 </Modal.Header>
                 <Modal.Body>
                     {
-                        ['Nitrogen', 'Phosphorus', 'Sediment'].map(name => {
+                        this.pollutants.map(pollutant => {
                             return (
-                                <div className={"container"}>
+                                <div className={"container"} key={pollutant.name + "-dashboard"}>
                                     <Col sm={3}>
                                         <h4 style={{ textAlign: "left" }}>
-                                            {name} Progress
+                                            {pollutant.name} Progress
                                         </h4>
                                         <div style={{ width: '100%', height: 200 }}>
                                             <ResponsiveContainer>
@@ -140,14 +117,14 @@ class SwammBmpChartClass extends React.Component {
                                     </Col>
                                     <Col sm={8}>
                                         <h4 style={{ textAlign: "center" }}>
-                                            { name } Load Reduction Totals
+                                            { pollutant.name } Load Reduction Totals
                                         </h4>
                                         <div style={{ width: '100%', height: 150 }}>
                                             <ResponsiveContainer>
                                                 <BarChart
                                                     width={600}
                                                     height={150}
-                                                    data={data3}
+                                                    data={data}
                                                     margin={{top: 10, right: 0, left: 0, bottom: 10}}
                                                     layout="vertical"
                                                     maxBarSize={150}
@@ -155,16 +132,27 @@ class SwammBmpChartClass extends React.Component {
                                                     <XAxis type="number" />
                                                     <YAxis type="category" hide/>
                                                     <CartesianGrid strokeDasharray="3 3" vertical={false} horizontal={false}/>
-                                                    <Tooltip content={
-                                                        <CustomTooltip/>
-                                                    }/>
-                                                    {data.map(obj => {
+                                                    <Tooltip
+                                                        content={<CustomTooltip
+                                                            tooltipBarId={this.state.tooltipBarId}
+                                                            tooltipPollutantKey={this.state.tooltipPollutantKey}
+                                                        />}
+                                                    />
+                                                    {Object.keys(data[0]).map(key => {
+                                                        const obj = data[0][key];
                                                         return (
                                                             <Bar
                                                                 key={obj.id}
                                                                 stackId="n"
-                                                                dataKey={(obj.id + ".n_red")}
+                                                                dataKey={(obj.id + "." + pollutant.load_red_total_key)}
                                                                 fill={colourMap[obj.type]}
+                                                                name="Name"
+                                                                onMouseOver={ () => {
+                                                                    this.setState({
+                                                                        tooltipBarId: obj.id,
+                                                                        tooltipPollutantKey: pollutant.load_red_total_key
+                                                                    });
+                                                                } }
                                                             />
                                                         );
                                                     })}
@@ -189,27 +177,44 @@ class SwammBmpChartClass extends React.Component {
             </Modal>
         );
     }
-    handleChange(event) {
-        console.log('handleChange', event);
-    }
+
+    pollutants = [
+        {
+            name: 'Nitrogen',
+            load_red_total_key: 'n_red'
+        },
+        {
+            name: 'Phosphorus',
+            load_red_total_key: 'p_red'
+        },
+        {
+            name: 'Sediment',
+            load_red_total_key: 's_red'
+        }
+    ]
 }
 
 class CustomTooltip extends React.Component {
     static propTypes = {
         type: PropTypes.string,
         payload: PropTypes.array,
-        label: PropTypes.string,
-        active: PropTypes.bool
+        active: PropTypes.bool,
+        tooltipBarId: PropTypes.string
     }
 
     render() {
-        const { active } = this.props;
-        if (active) {
-            const { payload, label } = this.props;
+        if (this.props.active) {
+            let type = null;
+            Object.keys(data[0]).map(key => {
+                const obj = data[0][key];
+                if (obj.id === this.props.tooltipBarId) {
+                    type =  obj.type;
+                }
+            });
             return (
                 <div className="custom-tooltip">
-                    <p className="label">{`${label} : ${payload[0].value}`}</p>
-                </div>
+                    <p className="label">{`${type}`}</p>
+                </div >
             );
         }
         return null;
