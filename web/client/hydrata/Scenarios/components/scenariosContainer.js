4727a5fd329fbdc363a6501c3d5447e011b3319b
davekennewell <davekennewell@gmail.com>
2021-04-19 14:28:17 +0400

    move scenarios menu

--- web/client/hydrata/Scenarios/components/scenariosContainer.js
+++ web/client/hydrata/Scenarios/components/scenariosContainer.js
@@ -127,7 +127,7 @@ class ScenariosContainer extends React.Component {
                     <ul className="menu-groups">
                         <button
                             key={'scenarios'}
-                            style={{...buttonStyle, left: (this.props.numberOfMenus + 1) * 100 + 20}}
+                            style={{...buttonStyle, left: (this.props.numberOfMenus + 5) * 100 + 20}}
                             onClick={() => {
                                 this.props.toggleScenarioManager();
                                 this.props.setMenuGroup('app_scenario');
